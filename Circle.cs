e7d3c9d2d868bf77129d702555c7b7107f960e66
benbryson789 <deborashelby@gmail.com>
2020-08-07 16:26:35 -0400

    first commit

---
+++ Circle.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Circle
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+            }
+        }
+        public double CalculateCircumference()
+        {
+            double Circumference;
+            Circumference = 2 * Math.PI * radius;
+            return Circumference;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCircumference());
+        }
+
+        public double CalculateArea()
+        {
+            double Area;
+            Area = Math.PI * radius * radius;
+            return Area;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            string TwoPlaces = String.Format("{0:.00}", x);
+            return TwoPlaces;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Radius;
+            string UserEnteredRadiusValue;
+            char RetryAgain;
+            int NoOfCircleObjects = 0;
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Enter radius: ");
+                UserEnteredRadiusValue = Console.ReadLine();
+
+                if (double.TryParse(UserEnteredRadiusValue, out Radius))
+                {
+                    Circle circle = new Circle(Radius);
+                    NoOfCircleObjects++;
+                    Console.WriteLine("Circumference: " + circle.CalculateFormattedCircumference() + " Area: " + circle.CalculateFormattedArea());
+
+                    Console.Write("Continue? (y/n): ");
+                    RetryAgain = Console.ReadKey(false).KeyChar;
+                    if ((RetryAgain == 'y') || (RetryAgain == 'Y'))
+                    {
+                        continue;
+                    }
+                    else if ((RetryAgain == 'n') || (RetryAgain == 'N'))
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Goodbye. You created " + NoOfCircleObjects + " Circle object(s).");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again");
+                    continue;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Program has exited. Press ENTER to close.");
+            Console.ReadLine();
+        }
+    }
+}
